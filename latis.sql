63c69aca359c58fb5c264f18a1dd60916cd9f726
suyaka2 <yossyrafli@gmail.com>
2026-09-18 10:38:40 +0700

    Menambahkan file database

---
+++ latis.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 18, 2026 at 03:37 AM
+-- Server version: 8.0.33
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `latis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lembaga`
+--
+
+CREATE TABLE `lembaga` (
+  `id_lembaga` varchar(6) NOT NULL,
+  `nama_lembaga` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `lembaga`
+--
+
+INSERT INTO `lembaga` (`id_lembaga`, `nama_lembaga`) VALUES
+('LBG001', 'Latis Education'),
+('LBG002', 'Tutor Indonesia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` varchar(6) NOT NULL,
+  `nama_siswa` varchar(50) NOT NULL,
+  `nis` varchar(10) NOT NULL,
+  `email_siswa` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `foto_siswa` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_lembaga` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nama_siswa`, `nis`, `email_siswa`, `foto_siswa`, `id_lembaga`) VALUES
+('SWA001', 'Rudi Santoso Hermanto', '1809500123', 'rudi_santoso@gmail.com', 'TESTER.jpg', 'LBG001'),
+('SWA002', 'Desta Maryanto', '2026090184', 'desta.botuna@gmail.com', 'air_force_one-removebg-preview_1.png', 'LBG002');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` varchar(6) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_kandidat` varchar(50) NOT NULL,
+  `foto_kandidat` varchar(20) NOT NULL,
+  `posisi_kandidat` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_kandidat`, `foto_kandidat`, `posisi_kandidat`) VALUES
+('USR001', 'admin1', '$2y$10$4Qe5WgyoN//YxolwipvRuec770pJUI/JSfx7ctlXfjmP5HRGDmkSW', 'Muhammad Rafli Suyaka Bintoro', 'Foto_Profile.jpg', 'IT Specialist');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lembaga`
+--
+ALTER TABLE `lembaga`
+  ADD PRIMARY KEY (`id_lembaga`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
